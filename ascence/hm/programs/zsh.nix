d659af4bf0b4612f56ac5c8714b9e2c01edbca47
razkaroth <rocker.ikaros@gmail.com>
2025-11-14 15:45:14 -0600

    implements supertouch

--- ascence/hm/programs/zsh.nix
+++ ascence/hm/programs/zsh.nix
@@ -43,6 +43,24 @@
       # Always mkdir a path (this doesn't inhibit functionality to make a single dir)
       alias mkdir='mkdir -p'
 
+      # Google calendar
+      alias gcal='gcalcli'
+      alias gcq='gcalcli --calendar rocker.ikaros@gmail.com quick'
+
+
+      # a super touch command
+      touch() {
+        for f in "$@"; do
+          if [[ "$f" == */ ]]; then
+            # Ends with a slash → make directory
+            mkdir -p "$f"
+          else
+            # Otherwise → make file (with parent dirs)
+            install -D /dev/null "$f"
+          fi
+        done
+      }
+
 
       export PATH=$HOME/.local/bin:$PATH
       export PATH="/home/raz/.cache/.bun/bin:$PATH"
