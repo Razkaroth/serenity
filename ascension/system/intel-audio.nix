889b63702e0d0aae04fbc4871b3398b31e52325b
razkaroth <rocker.ikaros@gmail.com>
2026-07-06 20:31:55 -0600

    fix ascension audio split

--- ascension/system/intel-audio.nix
+++ ascension/system/intel-audio.nix
@@ -1,4 +1,17 @@
 { ... }:
 {
   boot.kernelModules = [ "snd_sof_pci_intel_mtl" ];
+
+  services.pipewire.wireplumber.extraConfig."51-sof-soundwire" = {
+    "monitor.alsa.rules" = [
+      {
+        matches = [
+          { "device.name" = "alsa_card.pci-0000_00_1f.3-platform-sof_sdw"; }
+        ];
+        actions.update-props = {
+          "api.alsa.split-enable" = true;
+        };
+      }
+    ];
+  };
 }
