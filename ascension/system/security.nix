19b480c388f4d408b0c8b4867c8e0fdd5908c4f5
razkaroth <rocker.ikaros@gmail.com>
2026-07-20 18:17:57 -0600

    adds howdy to ascension

--- ascension/system/security.nix
+++ ascension/system/security.nix
@@ -1,5 +1,12 @@
 { ... }:
 {
+  services.howdy = {
+    enable = true;
+    # `sufficient` keeps password authentication available when recognition fails.
+    control = "sufficient";
+    settings.video.device_path = "/dev/v4l/by-path/pci-0000:00:14.0-usb-0:6:1.2-video-index0";
+  };
+
   security.pam.services.sddm = {
     enableGnomeKeyring = true;
   };
