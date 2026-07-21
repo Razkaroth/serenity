d3bc12c8c021da0a8c3b418b5a8aad65e8be751d
razkaroth <rocker.ikaros@gmail.com>
2026-07-20 20:37:38 -0600

    updates some flakes

--- ascension/system/security.nix
+++ ascension/system/security.nix
@@ -4,7 +4,10 @@
     enable = true;
     # `sufficient` keeps password authentication available when recognition fails.
     control = "sufficient";
-    settings.video.device_path = "/dev/v4l/by-path/pci-0000:00:14.0-usb-0:6:1.2-video-index0";
+    settings.video = {
+      device_path = "/dev/v4l/by-path/pci-0000:00:14.0-usb-0:6:1.2-video-index0";
+      timeout = 2;
+    };
   };
 
   security.pam.services.sddm = {
